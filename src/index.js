9a5eba41857e68c5ce9b50fa0ec1d87e2dae9e21
chekni <karina_cheshko@mail.ru>
2021-02-04 12:53:05 +0300

    fix: check on array and null

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
 exports.min = function min(array) {
-    if (Array.isArray(array) && array >= 0) {
-        let minimum = array[0];
-        array.forEach(element => {
-            if (minimum > element) {
-                minimum = element;
-            }
-        });
-        return minimum;
-    }
-    return 0;
+    if (!Array.isArray(array) || array <= 0) return 0;
+    let minimum = array[0];
+    array.forEach(element => {
+        if (minimum > element) {
+            minimum = element;
+        }
+    });
+    return minimum;
 }
 
 exports.max = function max(array) {
-    if (Array.isArray(array) && array >= 0) {
-        let maximum = array[0];
-        array.forEach(element => {
-            if (maximum < element) {
-                maximum = element;
-            }
-        });
-        return maximum;
-    }
-    return 0;
+    if (!Array.isArray(array) || array <= 0) return 0;
+    let maximum = array[0];
+    array.forEach(element => {
+        if (maximum < element) {
+            maximum = element;
+        }
+    });
+    return maximum;
 }
 
 exports.avg = function avg(array) {
-    if (Array.isArray(array) && array >= 0) return 0; {
-        let l = array.length;
-        let sum = 0;
-        array.forEach(elem => {
-            sum += elem;
-        })
-        return sum / l;
-    }
-    return 0;
+    if (!Array.isArray(array) || array <= 0) return 0;
+    let l = array.length;
+    let sum = 0;
+    array.forEach(elem => {
+        sum += elem;
+    })
+    return sum / l;
 }
